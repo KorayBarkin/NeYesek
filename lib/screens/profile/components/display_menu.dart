7fbd7d32c1fd1c0f3aac5be819be9c5b694b9127
brtozyildiz <beratozyildiz@gmail.com>
2021-05-07 22:16:01 +0300

    display menu is perfectly fine

--- lib/screens/profile/components/display_menu.dart
+++ lib/screens/profile/components/display_menu.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:food_ui_kit/components/buttons/primary_button.dart';
+import 'package:food_ui_kit/constants.dart';
+import 'package:food_ui_kit/size_config.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:food_ui_kit/screens/database/product.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'order_item_card.dart';
 
 class DisplayMenuScreen extends StatefulWidget {
   @override
@@ -8,6 +15,47 @@ class DisplayMenuScreen extends StatefulWidget {
 }
 
 class _DisplayMenuScreenState extends State<DisplayMenuScreen> {
+  List<Product> productList = [];
+  final auth = FirebaseAuth.instance;
+  final database = FirebaseDatabase.instance;
+  Product pro = new Product(
+      "values[key]['name']",
+      "values[key]['restaurantName']",
+      "values[key]['description']",
+      null,
+      "values[key]['category']",
+      13,
+      "values[key]['image']");
+
+  void initState() {
+    super.initState();
+    database
+        .reference()
+        .child("products/")
+        .once()
+        .then((DataSnapshot dataSnapshot) {
+      productList.clear();
+      var keys = dataSnapshot.value.keys;
+      var values = dataSnapshot.value;
+      for (var key in keys) {
+        if (values[key]['restaurantName'] == "CTIS Burger") {
+          Product product = new Product(
+              values[key]['name'],
+              values[key]['restaurantName'],
+              values[key]['description'],
+              null,
+              values[key]['category'],
+              values[key]['price'].toDouble(),
+              null);
+          productList.add(product);
+        }
+      }
+      setState(() {
+        //
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,18 +68,42 @@ class _DisplayMenuScreenState extends State<DisplayMenuScreen> {
             child: Image.asset("assets/images/NeYesek_banner.png"),
           ),
         ),
-        StreamBuilder(
-            stream:
-                FirebaseFirestore.instance.collection('products').snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData)
-                return Text("Loading... Please wait.");
-              else {
-                return Column(
-                  children: <Widget>[Text(snapshot.data.docs[0]['name'])],
-                );
-              }
-            })
+        SingleChildScrollView(
+          child: productList.length == 0
+              ? Center(
+                  child: Text(
+                  "Yüklenirken Lüften Bekleyiniz...",
+                  style: TextStyle(fontSize: 24),
+                ))
+              : Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  child: Column(
+                    children: [
+                      VerticalSpacing(),
+                      ...List.generate(
+                        productList.length,
+                        (index) => Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: kDefaultPadding / 2),
+                          child: OrderedItemCard(
+                            title: productList[index].name,
+                            description: productList[index].description,
+                            price: productList[index].price.toString() + " ₺",
+                          ),
+                        ),
+                      ),
+                      VerticalSpacing(of: 40),
+                      PrimaryButton(
+                        text: "Geri Dön",
+                        press: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+        ),
       ],
     ));
   }
